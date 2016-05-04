5afdc0910a0b38fc3b7c627bc1fac09b899b6111
haoyio <haoyi@stanford.edu>
2016-05-04 17:11:46 +0800

    updated makefile, expanded print board test cases

--- src/cpp/game/test.cpp
+++ src/cpp/game/test.cpp
@@ -1,9 +1,22 @@
+#include <vector>
+
 #include "../defines.h"
 #include "game.h"
 
+void test_print() {
+  std::vector<Board> boards = {
+    0x6621533332211111ull, 0x8643553333101100ull, 0x9653531032001001ull,
+   0x01231334047814cdull, 0x23212234346935cdull, 0x12231334248916cdull,
+    0x10022321345a34cdull, 0x11222234148a11cdull, 0x21121667028a20cdull
+  };
+
+  for (auto it = boards.begin(); it != boards.end(); ++it) {
+    print_board(*it);
+  }
+}
+
 int main() {
-  Board board = 0x12320448258924ADULL;
-  print_board(board);
+  test_print();
 
   return 0;
 }
