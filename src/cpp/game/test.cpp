45ea932f0ca063029d3c7163db77a4cf7716c0a2
haoyio <haoyi@stanford.edu>
2016-05-07 14:51:30 +0800

    changed user input game test

--- src/cpp/game/test.cpp
+++ src/cpp/game/test.cpp
@@ -7,7 +7,7 @@
 void test_print() {
   std::vector<Board> boards = {
     0x6621533332211111ull, 0x8643553333101100ull, 0x9653531032001001ull,
-   0x01231334047814cdull, 0x23212234346935cdull, 0x12231334248916cdull,
+    0x01231334047814cdull, 0x23212234346935cdull, 0x12231334248916cdull,
     0x10022321345a34cdull, 0x11222234148a11cdull, 0x21121667028a20cdull
   };
 
@@ -18,25 +18,25 @@ void test_print() {
 
 Action user_move(Board board) {
   print_board(board);
-  int move;
-  std::cout << "Please enter 0 (up), 1 (down), 2 (left), or 3 (right): ";
+  char move;
+  std::cout << "Please enter w (up), s (down), a (left), or d (right): ";
   std::cin >> move;
-  std::cout << "You input ";
+  std::cout << "You input: ";
   switch (move) {
-    case 0:
+    case 'w':
       std::cout << "up\n";
       return 0;
-    case 1:
+    case 's':
       std::cout << "down\n";
       return 1;
-    case 2:
+    case 'a':
       std::cout << "left\n";
       return 2;
-    case 3:
+    case 'd':
       std::cout << "right\n";
       return 3;
     default:
-      std::cout << "an invalid character, ending game...\n";
+      std::cout << move << "\nInvalid character, ending game...\n";
       return -1;
   }
 }
@@ -46,6 +46,7 @@ void test_game() {
 }
 
 int main() {
+  test_print();
   test_game();
 
   return 0;
